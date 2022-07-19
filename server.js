2498f4e2bbe8b0fec9c8ab1abf166ba31145e668
Danilo Palmeira <srdanlino@gmail.com>
2022-07-19 06:11:14 -0300

    Base da base criada

---
+++ server.js
@@ -0,0 +1,44 @@
+const net = require('net');
+const port = 777;
+const host = '127.0.0.1';
+
+const server = net.createServer();
+server.listen(port, host, () => {
+    console.log('Servidor rodando na porta ' + port + '.');
+});
+
+let sockets = [];
+
+server.on('connection', function(sock) {
+    console.log('Nova conexão [' + sock.remoteAddress + '] ' + sock.remotePort);
+    sockets.push(sock);
+
+    sock.on('data', async function(data) {
+        data = await decryptText(data.toString())
+        console.log('Recebido [' + sock.remoteAddress + '] ' + data);
+        
+        // COMPARTILHAR DADOS DE UM CLIENTE COM TODOS // APENAS EXEMPLO DE CHAT POR EXEMPLO
+        // sockets.forEach(function(sock, index, array) {
+        //     sock.write(sock.remoteAddress + ':' + sock.remotePort + " said " + data + '\n');
+        // });
+    });
+
+    // Remover cliente quando desconectado
+    sock.on('close', function(data) {
+        let index = sockets.findIndex(function(o) {
+            return o.remoteAddress === sock.remoteAddress && o.remotePort === sock.remotePort;
+        })
+        if (index !== -1) sockets.splice(index, 1);
+        console.log('Encerrou [' + sock.remoteAddress + '] ' + sock.remotePort);
+    });
+});
+
+async function decryptText(data){
+    var key = parseInt(data[0])
+    var data = data.slice(1).split('end~')[0].split('')
+    var ndata = ""
+    data.forEach(element => {
+        ndata += String.fromCharCode(element.charCodeAt(0)-key-1)
+    });
+    return ndata
+}
